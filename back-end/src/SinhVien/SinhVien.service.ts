913e086353b202e03f407d31dcdb3ef9ee557853
pmhanh <150933678+pmhanh@users.noreply.github.com>
2025-04-17 22:08:22 +0700

    add notification for academic year

--- back-end/src/SinhVien/SinhVien.service.ts
+++ back-end/src/SinhVien/SinhVien.service.ts
@@ -213,6 +213,16 @@ export class SinhVienService {
     );
   }
 
+  async addNotiToStudentsInNienKhoa(
+    NienKhoa: string,
+    thongBaoId: Types.ObjectId,
+  ): Promise<void> {
+    await this.sinhVienModel.updateMany(
+      { Khoa: NienKhoa },
+      { $push: { ThongBao: { thongBaoId, isRead: false } } },
+    );
+  }
+
   async removeNotiFromAll(thongBaoId: string): Promise<void> {
     await this.sinhVienModel.updateMany(
       {},
